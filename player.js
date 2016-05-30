88abb7fd282c20afd7bc2489f0a9de41ab50e068
Pool-q <aidenlevag@hotmail.sg>
2016-05-30 20:07:22 +0930

    player stuff

--- player.js
+++ player.js
@@ -5,10 +5,13 @@ var Player = function()
 	this.sprite.buildAnimation(1, 1, 120, 170, -1, [0]);
 	this.sprite.setAnimationOffset(0, 0, 0);
 	this.sprite.setLoop(0, false);
+	this.width = 120;
+	this.height = 170;
 	this.position = new Vector2();
-	this.position.set(500,200);
+	this.position.set(512 - this.width/2,300 - this.height/2);
 	this.velocity = new Vector2();
 	this.velocity.set(0,0);
+
 }
 
 Player.prototype.update = function(deltaTime)
